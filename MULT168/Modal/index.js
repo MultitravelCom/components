875c9604d6e2db677744f222034a3d17316f5dec
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-16 12:31:25 -0300

    0800

--- MULT168/Modal/index.js
+++ MULT168/Modal/index.js
@@ -12,45 +12,55 @@ function ButtonModal(props) {
 }
 
 const ModalContactos = () => {
-    <div className="main__container__modalContactos">
-        <div className="glyphicon glyphicon-phone"></div>
-        <div className="modalContactos__telephone">
-            <p>Ventas <span>0800 348 0003</span></p>
-            <p>Lunes a viernes de 10 a 20 hs</p>
-            <p>Sabados de 10 a 15 hs</p>
-            <span>Llamá al número en pantalla para que nuestros especialistas te asesoren con tu compra.</span>
+    return (
+        <div className="main__container__modalContactos">
+            <div className="glyphicon glyphicon-phone"></div>
+            <div className="modalContactos__telephone">
+                <p>Ventas <span>0800 348 0003</span></p>
+                <p>Lunes a viernes de 10 a 20 hs</p>
+                <p>Sabados de 10 a 15 hs</p>
+                <span>Llamá al número en pantalla para que nuestros especialistas te asesoren con tu compra.</span>
+            </div>
+            <ButtonModal
+                id={destino.title}
+                style="btn_Style_Venta_Per"
+                link={destino.linkWa}
+                text="Contactarme"
+            />
         </div>
-        <Button
-            id={destino.title}
-            style="btn_Style_Venta_Per"
-            link={destino.linkWa}
-            text="Contactarme"
-        />
-    </div>
-}
+    );
+};
 
 const Modal = () => {
     return (
         <>
             <div id="overlay" className="overlay">
                 <div className="container__modal">
-                    <span class="closeOverlay">X</span>
+                    <div className='emcabezadoModal'>
+                        <h3>Atención personalizada</h3>
+                        <span className="closeOverlay" onClick={closeModal}>X</span>
+                    </div>
                     <ModalContactos />
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 const Widget = () => {
+
     return (
         <>
-            <ButtonModal id="container__widget" className="main__container__widget">
+            <ButtonModal
+                id="container__widget"
+                className="main__container__widget"
+            >
                 <div className="glyphicon glyphicon-agent agentWidget"></div>
                 <div className="main__container__widget_text">Atención personalizada</div>
             </ButtonModal>
         </>
-    )
-}
+    );
+};
 
 ReactDOM.createRoot(document.getElementById('upper-menu')).render(<Widget />);
+s
