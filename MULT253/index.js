8267d67b9558aaa26d6ac12528099c9df4f578f5
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-17 12:21:56 -0300

    0800

---
+++ MULT253/index.js
@@ -0,0 +1,48 @@
+function Button(props) {
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setShowModal(true);
+    }
+
+    return (
+        <button id={props.id} className={props.style} onClick={handleClick}>{props.text}</button>
+    );
+}
+const BannerSearchResult = () => {
+    return (
+        <>
+            <div className="container_fluid">
+                <div className="main__container__text">
+                    <h2>¿Necesitas ayuda con tu reserva?</h2>
+                    <p>Contactate con nosotros a través del 0800-123-1234 (Venta) o al Whatsapp (PostVenta) para recibí atención personalizada</p>
+                </div>
+                <div className="main__container__img">
+                    <picture>
+                        <source media="(min-width: 1024px)" srcSet="https://multitravelcom.github.io/MT/Secciones/ResultadoBusqueda/busqueda.webp" />
+                        <source media="(min-width: 768px) and (max-width: 1023px)" srcSet="https://multitravelcom.github.io/MT/Secciones/ResultadoBusqueda/busqueda.webp" />
+                        <source media="(max-width: 767px)" srcSet="https://multitravelcom.github.io/MT/Secciones/ResultadoBusqueda/busqueda.webp" />
+                        <img className="main__container__img__img" src="https://multitravelcom.github.io/MT/Secciones/ResultadoBusqueda/busqueda.webp" alt="Imagen banner contacto personalizado" />
+                    </picture>
+                    <Button style="btn btnStyleBannerSearch" link="https://wa.link/j47nea" text="Contactarme" />
+                </div>
+            </div>
+        </>
+    );
+};
+const insertNewDivSearchResult = async () => {
+    while (true) {
+        const parentDiv = document.querySelector('.results-list__body');
+        const firstChdtDiv = document.querySelector('.results-list__item');
+
+        if (parentDiv && firstChdtDiv) {
+            const newDiv = document.createElement('div');
+            firstChdtDiv.after(newDiv);
+            ReactDOM.render(<BannerSearchResult />, newDiv);
+            break;
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+};
+insertNewDivSearchResult();
