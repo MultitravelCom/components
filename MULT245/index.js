ebd5fb382dd3c73bc449e6e186042a1e4ee69a02
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-17 15:51:39 -0300

    recomendado

--- MULT245/index.js
+++ MULT245/index.js
@@ -1,5 +1,5 @@
 function aplicarClaseRecomendada() {
-    const items = document.querySelectorAll('.results-list__item');
+    const items = document.querySelectorAll('.results-list__page');
   
     items.forEach(item => {
       const tieneDeals = item.querySelector('.info-card .info-card__content .deals') !== null;
