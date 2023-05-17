ef1615492a7f2c148dc68f0b3aacbecf61ab3936
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-17 16:34:53 -0300

    recomendado

--- MULT245/index.js
+++ MULT245/index.js
@@ -1,27 +1,44 @@
 async function aplicarClaseRecomendada() {
     const resultsListPage = document.querySelector('.results-list__page');
-  
+
     if (!resultsListPage) {
-      // No se encontró el elemento, espera un breve período y vuelve a intentar
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await aplicarClaseRecomendada();
-      return; // Salir de la función para evitar ejecutar el resto del código
+        // No se encontró el elemento, espera un breve período y vuelve a intentar
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await aplicarClaseRecomendada();
+        return; // Salir de la función para evitar ejecutar el resto del código
     }
-  
+
     const items = resultsListPage.querySelectorAll('.results-list__item');
-  
+
     items.forEach(item => {
         const tieneDeals = item.querySelector('.deals') !== null;
         const hotelResult = item.querySelector('.result.hotel-result');
-  
+
         if (tieneDeals && hotelResult) {
             hotelResult.classList.add('alojamiento-recomendado');
             console.log(tieneDeals);
-          }
+        }
     });
-  }
-  
-  document.addEventListener('DOMContentLoaded', async function() {
+}
+
+async function changeCopyMap() {
+    const resultsListPage = document.querySelector('.results-list__page');
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    if (!resultsListPage) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await aplicarClaseRecomendada();
+        return; 
+    }
+
+    items.forEach(item => {
+        const mapLink = item.querySelector('.map-link');
+        mapLink.textContent = 'Ver Mapa';
+        mapLink.style.display = 'block';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
     await aplicarClaseRecomendada();
-  });
-  
+    await changeCopyMap();
+});
