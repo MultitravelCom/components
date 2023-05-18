51eff8656dbbdf8540ac0b76eb5e181ffee2b810
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-18 20:07:52 -0300

    card alojamiento

--- MULT245/index.js
+++ MULT245/index.js
@@ -20,7 +20,7 @@ async function aplicarClaseRecomendada() {
 
         if (tieneDeals && hotelResult) {
             hotelResult.classList.add('alojamiento-recomendado');
-            tieneDeals.classList.add("info-card__price__deals");
+            item.classList.add("info-card__price__deals");
         }
     });
 }
