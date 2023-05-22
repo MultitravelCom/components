2b4185cd1449ae8577288f78841d717130c8ce74
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-22 12:54:43 -0300

    mult247

--- MULT247/index.js
+++ MULT247/index.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const descriptionSection = document.querySelector(".details-card__section.details-card__description");
     const content = descriptionSection.querySelector(".details-card__section-content");
-    const iconDiv = document.createElement('div'); 
-    iconDiv.className="glyphicon glyphicon-chevron-up";
+    const iconDiv = document.createElement('div');
+    iconDiv.className = "glyphicon glyphicon-chevron-down";
 
-    descriptionSection.addEventListener("click", function() {
-      content.style.display = (content.style.display === "none") ? "block" : "none";
+    descriptionSection.addEventListener("click", function () {
+        if (content.style.display === "none") {
+            content.style.display = "block";
+            icon.style.transform = "rotate(180deg)";
+        } else {
+            content.style.display = "none";
+            icon.style.transform = "rotate(0deg)";
+        }
     });
-  });
+
+    descriptionSection.appendChild(icon);
+});
