3ac6e1f79c450f41a819963d4e15fab4d90de761
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-23 15:11:48 -0300

    card alojamientos

--- MULT250/index.js
+++ MULT250/index.js
@@ -1,19 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const imageHolder = document.querySelector('.info-card__image-holder[data-title="Basia Hotel & Sushi"]');
-    if (imageHolder) {
-      const sources = imageHolder.querySelectorAll('source');
-      sources.forEach(function(source) {
-        source.srcset = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTERduDqa01YWeUfD7CwPH2SD41XriCvSNKVEQ7LzXV4A-OrO2TcM5noE02qVedYE7xQLY&usqp=CAU';
-      });
-    } else {
-      setTimeout(function() {
+document.addEventListener('DOMContentLoaded', async function () {
+    async function replaceImage() {
         const imageHolder = document.querySelector('.info-card__image-holder[data-title="Basia Hotel & Sushi"]');
         if (imageHolder) {
-          const sources = imageHolder.querySelectorAll('source');
-          sources.forEach(function(source) {
-            source.srcset = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTERduDqa01YWeUfD7CwPH2SD41XriCvSNKVEQ7LzXV4A-OrO2TcM5noE02qVedYE7xQLY&usqp=CAU';
-          });
+            const sources = imageHolder.querySelectorAll('source');
+            sources.forEach(function (source) {
+                source.srcset = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTERduDqa01YWeUfD7CwPH2SD41XriCvSNKVEQ7LzXV4A-OrO2TcM5noE02qVedYE7xQLY&usqp=CAU';
+            });
+        } else {
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Esperar 1000ms
+            await replaceImage(); // Volver a llamar a la función
         }
-      }, 100);
     }
-  });
+
+    await replaceImage();
+});
