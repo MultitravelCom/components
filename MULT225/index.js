feb18cbb867d9a3359e644b9c7da9215a0c55beb
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-05-23 16:24:14 -0300

    card alojamientos

--- MULT225/index.js
+++ MULT225/index.js
@@ -82,4 +82,4 @@ const BannerMediosDePagos = () => {
     )
 }
 
-ReactDOM.render(<BannerMediosDePagos />, newDiv);
+ReactDOM.createRoot(document.getElementById('rootHome')).render(<BannerMediosDePagos />);
---
+++ MULT251/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const liElement = document.querySelector('.booking-payment__options-prepayment');
+    const bookingPaymentOptions = document.querySelector('.booking-payment__options');
+
+    if (liElement) {
+        liElement.childNodes.forEach(node => {
+            if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() === 'Transferencia') {
+                node.textContent = 'Transferencia / Efectivo';
+                bookingPaymentOptions.style.display = 'inline-block';
+            }
+        });
+    }
+});
