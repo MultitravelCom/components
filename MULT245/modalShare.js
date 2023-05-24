7fd00004878dcaa6210cfc2abf73c91b896062bb
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-24 13:11:59 -0300

    alojamientos

--- MULT245/modalShare.js
+++ MULT245/modalShare.js
@@ -1,3 +1,128 @@
+function wait(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
+async function aplicarClaseRecomendada() {
+    const resultsListPage = document.querySelector('.results-list__page');
+
+    if (!resultsListPage) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await aplicarClaseRecomendada();
+        return;
+    }
+
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        const tieneDeals = item.querySelector('.deals') !== null;
+        const hotelResult = item.querySelector('.result.hotel-result');
+
+        if (tieneDeals && hotelResult) {
+            hotelResult.classList.add('alojamiento-recomendado');
+            const bestPriceElements = item.querySelectorAll('.info-card__price');
+            bestPriceElements.forEach(element => {
+                element.classList.add("info-card__price__deals");
+            });
+        }
+    });
+}
+
+async function agreeStarIcon() {
+    const resultsListPage = document.querySelector('.results-list__page');
+
+    while (!resultsListPage) {
+        await wait(1000);
+        resultsListPage = document.querySelector('.results-list__page');
+    }
+
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        const infoCardCategory = item.querySelector('.info-card__category');
+
+        const newSpaninfoCardCategory = document.createElement('span');
+        newSpaninfoCardCategory.className = 'glyphicon glyphicon-star iconStar';
+
+        infoCardCategory.insertBefore(newSpaninfoCardCategory, infoCardCategory.firstChild);
+    });
+}
+
+async function changeCopyMap() {
+    const resultsListPage = document.querySelector('.results-list__page');
+
+    while (!resultsListPage) {
+        await wait(1000);
+        resultsListPage = document.querySelector('.results-list__page');
+    }
+
+    const items = resultsListPage.querySelectorAll('.results-list__item');
+
+    items.forEach(item => {
+        const mapLink = item.querySelector('.map-link');
+        const locationIcon = mapLink.querySelector('.info-card__location-icon');
+
+        mapLink.lastChild.textContent = 'Ver Mapa';
+        mapLink.style.display = 'block';
+    });
+}
+
+async function applyDisplayNoneToAllButLastButton() {
+    const actionsContainer = document.querySelector('.info-card__actions');
+    const buttonsAndLinks = actionsContainer.querySelectorAll('button, a');
+
+    for (let i = 0; i < buttonsAndLinks.length; i++) {
+        if (i !== buttonsAndLinks.length - 1) {
+            buttonsAndLinks[i].style.display = 'none';
+        }
+    }
+
+    const lastButtonContainer = buttonsAndLinks[buttonsAndLinks.length - 1].parentNode;
+    lastButtonContainer.style.display = 'inline-block';
+
+    const buttonsVerDetalle = document.querySelectorAll('.info-card__options-toggle');
+
+    for (let i = 0; i < buttonsVerDetalle.length; i++) {
+        buttonsVerDetalle[i].textContent = 'Ver detalle';
+    }
+}
+async function changeCopyButton() {
+    const resultsListPage = document.querySelector('.results-list__page');
+
+    while (!resultsListPage) {
+        await wait(1000);
+        resultsListPage = document.querySelector('.results-list__page');
+    }
+    const itemsButtonComprar = resultsListPage.querySelectorAll('.results-list__item');
+
+    itemsButtonComprar.forEach(item => {
+        const buttonElement = item.querySelector('.info-card__options-toggle');
+        buttonElement.textContent = 'Comprar';
+        buttonElement.style.display = 'block';
+    });
+
+    const checkResultsListPage = () => {
+
+        const resultsPage = document.querySelector('.results-list__page');
+
+        if (resultsPage) {
+            const resultsListPage = resultsPage.querySelectorAll('.results-list__item');
+
+            resultsListPage.forEach(item => {
+                const selectors = item.querySelectorAll('.info-card__image-holder');
+
+                selectors.forEach(selector => {
+                    selector.classList.remove('js-open-gallery');
+                });
+            });
+        } else {
+            setTimeout(checkResultsListPage, 2000);
+        }
+    };
+
+    checkResultsListPage();
+};
+
+
 function ButtonModalShare(props) {
     const handleClick = (event) => {
         event.preventDefault();
@@ -55,33 +180,33 @@ const BannerMensageCardApp = () => {
 
     React.useEffect(() => {
         const checkDivPresence = () => {
-          const div = document.querySelector('.bestprice__taxincluded');
-          setHasBestPriceTaxIncluded(!div);
+            const div = document.querySelector('.bestprice__taxincluded');
+            setHasBestPriceTaxIncluded(!div);
         };
-    
+
         const applyDOMChanges = async () => {
-          await aplicarClaseRecomendada();
-          await changeCopyMap();
-          await applyDisplayNoneToAllButLastButton();
-          await agreeStarIcon();
-          await changeCopyButton();
+            await aplicarClaseRecomendada();
+            await changeCopyMap();
+            await applyDisplayNoneToAllButLastButton();
+            await agreeStarIcon();
+            await changeCopyButton();
         };
-    
+
         const observer = new MutationObserver(applyDOMChanges);
         const targetNode = document.querySelector('.results-list__page');
-    
+
         if (targetNode) {
-          observer.observe(targetNode, { childList: true, subtree: true });
+            observer.observe(targetNode, { childList: true, subtree: true });
         }
-    
+
         checkDivPresence();
         const interval = setInterval(checkDivPresence, 1000);
-    
+
         return () => {
-          clearInterval(interval);
-          observer.disconnect();
+            clearInterval(interval);
+            observer.disconnect();
         };
-      }, []);
+    }, []);
 
     return (
         <>
