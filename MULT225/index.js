b8569b1e6d9100ae5bddb6672aef4bc07f6924d8
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-24 17:28:17 -0300

    alojamientos

--- MULT225/index.js
+++ MULT225/index.js
@@ -81,10 +81,10 @@ const BannerMediosDePagos = () => {
     return (
         <div className="main__seccion__pagos container ">
             <div className="row align-items-start">
-                <div className="col">
+                <div className="col-md-6 col-xs-12">
                     <SeccionMediosDePagos showImageFirst={true} title="Vos decidís" text_p="Encontrá todos los medios de pagos exclusivos para vos" />
                 </div>
-                <div className="col">
+                <div className="col-md-6 col-xs-12">
                     <SeccionMediosDePagos showImageFirst={false} title="Quienes somos" text_p="Vení a descubrir el MultiTeam pa" />
                 </div>
 
