1ac847465557e765b8b5b2419f5ce06c34f42daa
Multitravel.com <ignacio.peralta@multitravel.com>
2023-05-31 16:59:07 -0300

    mult263

---
+++ MULT263/index.js
@@ -0,0 +1,48 @@
+function ButtonBannerDescuento(props) {
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setShowModal(true);
+    }
+
+    return (
+        <button id={props.id} className={props.style} onClick={handleClick}>{props.text}</button>
+    );
+}
+const DescuentoComponente = () => {
+    return (
+        <div className="main__container_excluvisa">
+            <div className="main__container_excluvisa__text">
+                <div className="main__container_excluvisa__text__h1">
+                    <h1>¡OFERTA EXCLUSIVA!</h1>
+                </div>
+                <div className="main__container_excluvisa__text__p">
+                    <p>Contactate con nuestros especialistas en viajes y ahorrá hasta un 10%.</p>
+                </div>
+            </div>
+            <div className="main__container_excluvisa__img">
+                <img src=""></img>
+            </div>
+            <ButtonBannerDescuento id="btnBannerSearch" style="btn btnStyleBannerSearch" link="https://wa.link/j47nea" text="Llamá al 0800 348 0003" />
+        </div>
+    )
+}
+
+
+const insertNewComponent = async () => {
+    while (true) {
+        const parentDivDescuentoComponente = document.querySelector('.details-card__top');
+        const firstChildDescuentoComponente = parentDivDescuentoComponente?.firstElementChild;
+
+        if (parentDivDescuentoComponente && firstChildDescuentoComponente) {
+            const newDivDescuentoComponente = document.createElement('div');
+            firstChild.after(newDivDescuentoComponente);
+            ReactDOM.render(<DescuentoComponente />, newDivDescuentoComponente);
+            break;
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+};
+
+insertNewComponent();
