7583b4b1db72a9c4cd82fafe99a1950c60e44895
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-01 17:52:40 -0300

    mult263

--- MULT276/index.js
+++ MULT276/index.js
@@ -1,45 +1,50 @@
 const CopyTaxFlight = () => {
-    return (
-      <div className="main_container_copyTaxFlight">
-        <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
-        <p className="main__warningPric__icon__p">
-          En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.
-        </p>
-      </div>
-    );
-  };
+  return (
+    <div className="main_container_copyTaxFlight">
+      <div className="main__warningPric__icon glyphicon glyphicon-info-circle"></div>
+      <p className="main__warningPric__icon__p">
+        En caso de vuelos internacionales, los precios siempre incluyen impuesto país y percepciones.
+      </p>
+    </div>
+  );
+};
 
-  function renderCopyTaxFlight() {
-    const flightSelectionElement = document.querySelector('.flight-selection');
-    if (flightSelectionElement) {
-      const copyTaxFlightContainer = document.createElement('div');
-      copyTaxFlightContainer.classList.add('copy-tax-flight-container');
-      flightSelectionElement.appendChild(copyTaxFlightContainer);
-  
-      ReactDOM.render(<CopyTaxFlight />, copyTaxFlightContainer);
-    }
+function renderCopyTaxFlight() {
+  const flightSelectionElement = document.querySelector('.flight-selection');
+  if (flightSelectionElement) {
+    const copyTaxFlightContainer = document.createElement('div');
+    copyTaxFlightContainer.classList.add('copy-tax-flight-container');
+    flightSelectionElement.appendChild(copyTaxFlightContainer);
+
+    ReactDOM.render(<CopyTaxFlight />, copyTaxFlightContainer);
   }
-
-  
-  function observarCambiosVirtualDOM() {
-    const observerConfig = {
-        rootNode: document.documentElement,
-        callback: (summaries) => {
-            summaries.forEach((summary) => {
-                const addedNodes = Array.from(summary.added);
-                const placeholderDiv = addedNodes.find(node => node.classList && node.classList.contains('js-results-list-selection-placeholder'));
-
-                if (placeholderDiv) {
-                    renderCopyTaxFlight();
-                }
-            });
-        },
-        queries: [{ element: '.js-results-list-selection-placeholder' }],
-    };
-
-    const observer = new MutationSummary(observerConfig);
 }
 
-document.addEventListener('DOMContentLoaded', async function () {
-    observarCambiosVirtualDOM();
+
+function observarComponenteEnDOM() {
+  const observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+      const addedNodes = mutation.addedNodes;
+      for (let node of addedNodes) {
+        if (node.classList && node.classList.contains('js-results-list-selection-placeholder')) {
+          console.log('El componente está presente en el DOM');
+          ejecutarFuncion();
+          return; // Termina el bucle si se encuentra el componente
+        }
+      }
+    }
+  });
+
+  const observerConfig = {
+    childList: true,
+    subtree: true,
+  };
+
+  const targetNode = document.documentElement; // Puedes ajustar el selector según tu estructura
+
+  observer.observe(targetNode, observerConfig);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  observarComponenteEnDOM();
 });
