8e3d7c3a7d2df82c91997036fc23c2ba94277a41
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-01 22:21:17 -0300

    mult263

--- MULT258/index.js
+++ MULT258/index.js
@@ -1,20 +1,20 @@
+let bannerToastify = document.createElement('div');
+bannerToastify.className = 'custom-div';
+bannerToastify.innerHTML =
+    `
+  <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
+  <div class="custom-content">
+    <h2>Lo que ves, es lo que pagás</h2>
+    <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
+  </div>
+`
 
-    let bannerToastify = document.createElement('div');
-    bannerToastify.className = 'custom-div';
-    bannerToastify.innerHTML = `
-      <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
-      <div class="custom-content">
-        <h2>Lo que ves, es lo que pagás</h2>
-        <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
-      </div>
-    `;
-
-    Toastify({
-        node: bannerToastify,
-        duration: 10000,
-        gravity: 'bottom',
-        position: 'left',
-        style: {
-            background: '#2096ff'
-        }
-    }).showToast();
+Toastify({
+    node: bannerToastify,
+    duration: 10000,
+    gravity: 'bottom', // Puedes ajustar la posición según tus necesidades
+    position: 'left', // Puedes ajustar la posición según tus necesidades
+    style: {
+        background: '#2096ff'
+    }
+}).showToast();
