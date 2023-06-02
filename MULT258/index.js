6555274b27fd74725e2e84af54cbbee70144b8fb
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-01 21:18:15 -0300

    mult263

--- MULT258/index.js
+++ MULT258/index.js
@@ -1,7 +1,19 @@
+let bannerToastify = document.createElement('div');
+bannerToastify.className = 'custom-div';
+bannerToastify.innerHTML =
+    `
+  <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
+  <div class="custom-content">
+    <h2>Lo que ves, es lo que pagás</h2>
+    <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
+  </div>
+`
+
+
 Toastify({
-    text: "Lo que ves, es lo que pagás",
+    node: bannerToastify,
     duration: 10000,
     gravity: 'bottom', // Puedes ajustar la posición según tus necesidades
     position: 'right', // Puedes ajustar la posición según tus necesidades
-    backgroundColor: '#ffffff'
-    }).showToast();
+    backgroundColor: '#2096ff'
+}).showToast();
