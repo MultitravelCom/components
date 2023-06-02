2e1e9e443081eb2fc82ef26d511a907e344ab22b
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-01 22:12:27 -0300

    mult263

--- MULT258/index.js
+++ MULT258/index.js
@@ -1,18 +1,19 @@
-let bannerToastify = document.createElement('div');
-bannerToastify.className = 'custom-div';
-bannerToastify.innerHTML =
-    `
-  <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
-  <div class="custom-content">
-    <h2>Lo que ves, es lo que pagás</h2>
-    <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
-  </div>
-`
-
-Toastify({
-    node: bannerToastify,
-    duration: 10000,
-    gravity: 'bottom', // Puedes ajustar la posición según tus necesidades
-    position: 'left', // Puedes ajustar la posición según tus necesidades
-    backgroundColor: '#2096ff'
-}).showToast();
+document.addEventListener('DOMContentLoaded', function() {
+    let bannerToastify = document.createElement('div');
+    bannerToastify.className = 'custom-div';
+    bannerToastify.innerHTML = `
+      <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
+      <div class="custom-content">
+        <h2>Lo que ves, es lo que pagás</h2>
+        <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
+      </div>
+    `;
+  
+    Toastify({
+      node: bannerToastify,
+      duration: 10000,
+      gravity: 'bottom',
+      position: 'left',
+      backgroundColor: '#2096ff'
+    }).showToast();
+  });
