6701800344599516daeda912dfb4aa18003f70ea
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-01 21:09:59 -0300

    mult263

--- MULT258/index.js
+++ MULT258/index.js
@@ -1,7 +1,7 @@
 Toastify({
     text: "Lo que ves, es lo que pagás",
     duration: 10000,
-    gravity: 'botton', // Puedes ajustar la posición según tus necesidades
+    gravity: 'bottom', // Puedes ajustar la posición según tus necesidades
     position: 'right', // Puedes ajustar la posición según tus necesidades
     backgroundColor: '#ffffff'
     }).showToast();
