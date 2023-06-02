6b0067ba5628ceb9ff901ab90e9e576f7d7c8297
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-02 10:01:46 -0300

    toastify

--- MULT258/index.js
+++ MULT258/index.js
@@ -1,20 +1,24 @@
-let bannerToastify = document.createElement('div');
-bannerToastify.className = 'custom-div';
-bannerToastify.innerHTML =
-    `
-  <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
-  <div class="custom-content">
-    <h2>Lo que ves, es lo que pagás</h2>
-    <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
-  </div>
-`
+window.onload = function() {
+  // Crear el elemento de banner de Toastify
+  let bannerToastify = document.createElement('div');
+  bannerToastify.className = 'custom-div';
+  bannerToastify.innerHTML =
+      `
+    <img src="ruta-de-la-imagen.jpg" alt="Imagen" class="custom-img">
+    <div class="custom-content">
+      <h2>Lo que ves, es lo que pagás</h2>
+      <p>Precios finales en todos nuestros productos, impuestos incluidos. Sin costos extras en aeropuerto o destino.</p>
+    </div>
+  `;
 
-Toastify({
+  // Configurar y mostrar el toast de Toastify
+  Toastify({
     node: bannerToastify,
     duration: 10000,
     gravity: 'bottom', // Puedes ajustar la posición según tus necesidades
     position: 'left', // Puedes ajustar la posición según tus necesidades
     style: {
-        background: '#2096ff',
+      background: '#2096ff',
     }
-}).showToast();
+  }).showToast();
+}
