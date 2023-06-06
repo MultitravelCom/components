4bc29b004156e1813cccc94deacaea90572fe42f
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-06 20:54:06 -0300

    mult163

--- MULT163/botonUser.js
+++ MULT163/botonUser.js
@@ -2,7 +2,7 @@ const targetElementUser = document.querySelector('#main-menu-features');
 
 const buttonHTML = `
   <button type="button" class="btn">
-    <span class="glyphicon glyphicon-agent agentWidget" aria-hidden="true"></span>
+    <span class="glyphicon glyphicon-agent agentWidget"></span>
     <span class="sr-only">Ingreso</span>
     <span class="quicklink-elem-label">Ingreso</span>
   </button>
