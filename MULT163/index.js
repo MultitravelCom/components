3848a2b4e2d318d4257c074dac39aeb326d12c5d
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-06 13:17:43 -0300

    mult163

--- MULT163/index.js
+++ MULT163/index.js
@@ -1,7 +1,7 @@
 const ButtonUserMobile = () => {
     return (
         <>
-            <button type="button" className="btn btn-secondary upper-menu__login-button js-login-box-modal hidden-xs" data-toggle="modal">
+            <button type="button" className="btn upper-menu__newsletter-button features_item js-login-box-modal hidden-xs" data-toggle="modal">
                 <span className="glyphicon glyphicon-agent agentWidget" aria-hidden="true"></span>
                 <span className="sr-only">Ingreso</span>
                 <span className="quicklink-elem-label">Ingreso</span>
