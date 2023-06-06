d8ae6a84ff79b5e14764ca5d7b5c20e674f113ca
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-06 16:29:14 -0300

    mult163

--- MULT163/index.js
+++ MULT163/index.js
@@ -13,7 +13,7 @@
 const targetElementUser = document.querySelector('#main-menu-features');
 
 const buttonHTML = `
-  <button type="button" class="btn upper-menu__newsletter-button features_item js-login-box-modal" data-toggle="modal">
+  <button type="button" class="btn features_item js-login-box-modal" data-toggle="modal">
     <span class="glyphicon glyphicon-agent agentWidget" aria-hidden="true"></span>
     <span class="sr-only">Ingreso</span>
     <span class="quicklink-elem-label">Ingreso</span>
