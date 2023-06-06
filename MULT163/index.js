5d3c0df29f2b9cb64828960966544892fcf49357
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-06 16:44:48 -0300

    mult163

--- MULT163/index.js
+++ MULT163/index.js
@@ -10,21 +10,6 @@
 //     )
 // }
 
-const targetElementUser = document.querySelector('#main-menu-features');
-
-const buttonHTML = `
-  <button type="button" class="btn upper-menu__newsletter-button features_item js-login-box-modal">
-    <span class="glyphicon glyphicon-agent agentWidget" aria-hidden="true"></span>
-    <span class="sr-only">Ingreso</span>
-    <span class="quicklink-elem-label">Ingreso</span>
-  </button>
-`;
-
-targetElementUser.insertAdjacentHTML('beforeend', buttonHTML);
-
-
-
-
 const ButtonARSEs = () => {
     return (
         <div className="main_container__buttonARSEs">
