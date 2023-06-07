d8b3acff0b88d45420bf3ca9b0220535dccd1116
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-06 21:14:09 -0300

    mult163

--- MULT163/botonUser.js
+++ MULT163/botonUser.js
@@ -1,20 +1,19 @@
-const targetElementUser = document.querySelector('#main-menu-features');
+// const targetElementUser = document.querySelector('#main-menu-features');
 
-const buttonHTML = `
-<div class="upper-menu_quicklinks btn-toolbar visible-xs-block visible-sm-block">
-  <button type="button" class="btn upper-menu__newsletter-button features_item js-login-box-modal btn-user">
-    <span class="glyphicon glyphicon-agent" aria-hidden="true"></span>
-    <span class="sr-only">Ingreso</span>
-    <span class="quicklink-elem-label">Ingreso</span>
-  </button>
-</div>
-`;
+// const buttonHTML = `
+// <div class="upper-menu_quicklinks btn-toolbar visible-xs-block visible-sm-block">
+//   <button type="button" class="btn upper-menu__newsletter-button features_item js-login-box-modal btn-user">
+//     <span class="glyphicon glyphicon-agent" aria-hidden="true"></span>
+//     <span class="sr-only">Ingreso</span>
+//     <span class="quicklink-elem-label">Ingreso</span>
+//   </button>
+// </div>
+// `;
 
-targetElementUser.insertAdjacentHTML('beforeend', buttonHTML);
+// targetElementUser.insertAdjacentHTML('beforeend', buttonHTML);
 
-const buttonsBtnUser = targetElementUser.querySelectorAll('.btn-user');
+// const buttonsBtnUser = targetElementUser.querySelectorAll('.btn-user');
 
-// Si hay más de un botón, ocultar el segundo
-if (buttonsBtnUser.length > 1) {
-    buttonsBtnUser[1].classList.add('hidden');
-}
+// if (buttonsBtnUser.length > 1) {
+//     buttonsBtnUser[1].classList.add('hidden');
+// }
