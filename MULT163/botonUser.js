a80e37b1c8584fbf68b264e893d03f31ae59e00f
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-06 21:03:55 -0300

    mult163

--- MULT163/botonUser.js
+++ MULT163/botonUser.js
@@ -1,7 +1,7 @@
 const targetElementUser = document.querySelector('#main-menu-features');
 
 const buttonHTML = `
-  <button type="button" class="btn upper-menu__newsletter-button features_item js-login-box-modal">
+  <button type="button" class="btn upper-menu__newsletter-button features_item js-login-box-modal btn-usd">
     <span class="glyphicon glyphicon-agent" aria-hidden="true"></span>
     <span class="sr-only">Ingreso</span>
     <span class="quicklink-elem-label">Ingreso</span>
@@ -9,3 +9,10 @@ const buttonHTML = `
 `;
 
 targetElementUser.insertAdjacentHTML('beforeend', buttonHTML);
+
+const buttonsBtnUser = targetElementUser.querySelectorAll('.btn-usd');
+
+// Si hay más de un botón, ocultar el segundo
+if (buttonsBtnUser.length > 1) {
+    buttonsBtnUser[1].classList.add('hidden');
+}
