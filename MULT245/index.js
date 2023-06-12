4afbabed2937002f6cf15cad85e2acdb075c4054
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-12 16:09:53 -0300

    mult308

--- MULT245/index.js
+++ MULT245/index.js
@@ -175,4 +175,5 @@ function observarCambiosCheckAndRender() {
 document.addEventListener('DOMContentLoaded', async function () {
     observarCambiosCheckAndRender();
     cargarEstilosYModales();
+    removeImageLinks(resultsListPage);
 });
