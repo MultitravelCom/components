456b35237951856d4059a9b3eb99d1cea4ba5ab8
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-14 12:34:22 -0300

    mult269

--- MULT269/index.js
+++ MULT269/index.js
@@ -1,33 +1,9 @@
-// Función para cambiar el texto del mapa
-function cambiarTextoMapa(span) {
-    if (span.innerText === 'Abrir mapa') {
-      span.innerText = 'Ver en mapa';
-    }
-  }
-  
-  // Función para iterar y cambiar el texto del botón en .results__sidebar
-  function cambiarTextoBoton() {
-    let sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map');
-    sidebarElements.forEach(function(element) {
-      var span = element.querySelector('span.btn.btn-secondary');
-      cambiarTextoMapa(span);
+function cambiarTextoMapaYBoton() {
+    var sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map, .js-sidebar-map-placeholder .sidebar-map');
+    sidebarElements.forEach(function (element) {
+        const span = element.querySelector('span.btn.btn-secondary');
+        if (span.innerText === 'Abrir mapa') {
+            span.innerText = 'Ver en mapa';
+        }
     });
-  }
-  
-  // Configuración para MutationSummary
-  let observerConfig = {
-    callback: function(summaries) {
-      cambiarTextoBoton();
-    },
-    queries: [{
-      element: '.results__sidebar',
-      elementAttributes: 'class'
-    }]
-  };
-  
-  // Crear una nueva instancia de MutationSummary
-  let observer = new MutationSummary(observerConfig);
-  
-  // Llamar a la función inicial para cambiar el texto del botón
-  cambiarTextoBoton();
-  
+}
