474d26e859fd19ab8385ff7e8fce12ccfd194dbe
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-14 12:10:18 -0300

    mult269

--- MULT269/index.js
+++ MULT269/index.js
@@ -1,12 +1,12 @@
 // Función para cambiar el texto del mapa
 function cambiarTextoMapa(span) {
-    if (span.innerText === 'Abrir el mapa') {
+    if (span.innerText === 'Abrir mapa') {
       span.innerText = 'Ver en mapa';
     }
   }
   
   // Configuración para MutationSummary
-  var observerConfig = {
+  let observerConfig = {
     callback: function(summaries) {
       summaries[0].added.forEach(function(element) {
         if (element.classList.contains('sidebar-map')) {
@@ -25,6 +25,6 @@ function cambiarTextoMapa(span) {
   var observer = new MutationSummary(observerConfig);
   
   // Llamar a la función inicial para cambiar el texto del mapa
-  var span = document.querySelector('.results__sidebar .sidebar-map span.btn.btn-secondary');
+  let span = document.querySelector('.results__sidebar .sidebar-map span.btn.btn-secondary');
   cambiarTextoMapa(span);
   
