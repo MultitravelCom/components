b3352ff38b5ede7d0ff93dc473730cdb92d8ed21
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-14 12:40:05 -0300

    mult269

--- MULT269/index.js
+++ MULT269/index.js
@@ -1,25 +1,13 @@
-// Función para cambiar el texto del mapa y del botón
 function cambiarTextoMapaYBoton() {
-    var sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map, .js-sidebar-map-placeholder .sidebar-map');
-    sidebarElements.forEach(function(element) {
-      var span = element.querySelector('span.btn.btn-secondary');
-      if (span.innerText === 'Abrir mapa') {
-        span.innerText = 'Ver en mapa';
-      }
+    let sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map, .js-sidebar-map-placeholder .sidebar-map');
+    sidebarElements.forEach(function (element) {
+        var span = element.querySelector('span.btn.btn-secondary');
+        if (span.innerText === 'Abrir mapa') {
+            span.innerText = 'Ver en mapa';
+        }
     });
-  }
-  
-  // Función para esperar a que se cargue la página y buscar el selector
-  function esperarYBuscarSelector() {
-    var interval = setInterval(function() {
-      var sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map, .js-sidebar-map-placeholder .sidebar-map');
-      if (sidebarElements.length > 0) {
-        clearInterval(interval);
-        cambiarTextoMapaYBoton();
-      }
-    }, 1000);
-  }
-  
-  // Esperar a que se cargue completamente la página
-  document.addEventListener('DOMContentLoaded', esperarYBuscarSelector);
-  
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    cambiarTextoMapaYBoton();
+});
