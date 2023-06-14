faa6fe08bb2c401d87e4851d86ae1ee04db42afd
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-14 12:36:35 -0300

    mult269

--- MULT269/index.js
+++ MULT269/index.js
@@ -1,9 +1,25 @@
+// Función para cambiar el texto del mapa y del botón
 function cambiarTextoMapaYBoton() {
     var sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map, .js-sidebar-map-placeholder .sidebar-map');
-    sidebarElements.forEach(function (element) {
-        const span = element.querySelector('span.btn.btn-secondary');
-        if (span.innerText === 'Abrir mapa') {
-            span.innerText = 'Ver en mapa';
-        }
+    sidebarElements.forEach(function(element) {
+      var span = element.querySelector('span.btn.btn-secondary');
+      if (span.innerText === 'Abrir mapa') {
+        span.innerText = 'Ver en mapa';
+      }
     });
-}
+  }
+  
+  // Función para esperar a que se cargue la página y buscar el selector
+  function esperarYBuscarSelector() {
+    var interval = setInterval(function() {
+      var sidebarElements = document.querySelectorAll('.results__sidebar .sidebar-map, .js-sidebar-map-placeholder .sidebar-map');
+      if (sidebarElements.length > 0) {
+        clearInterval(interval);
+        cambiarTextoMapaYBoton();
+      }
+    }, 1000);
+  }
+  
+  // Esperar a que se cargue completamente la página
+  document.addEventListener('DOMContentLoaded', esperarYBuscarSelector);
+  
