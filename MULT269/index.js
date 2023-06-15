ea60e4a5895ec05335b76257a618a8e6031b75a1
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-15 18:11:48 -0300

    mult269

--- MULT269/index.js
+++ MULT269/index.js
@@ -113,33 +113,13 @@ function agregarNewsButtons() {
         });
     });
 
-    const sidebarFilters = document.querySelector('.sidebar-filters');
-    const buttonFilter = buttonsMapFilter.querySelector('.main__container__newsButtons');
-
-    // Función para verificar si el sidebar-filters está presente y visible
-    function isSidebarFiltersVisible() {
-        return sidebarFilters && sidebarFilters.offsetLeft === 0;
-    }
-
-    // Función para actualizar el z-index del botón en función del estado del sidebar-filters
-    function updateButtonFilterZIndex() {
-        if (isSidebarFiltersVisible()) {
-            buttonFilter.style.zIndex = '0';
-        } else {
-            buttonFilter.style.zIndex = '9999999';
-        }
-    }
-
-    // Llamar a la función para actualizar el z-index inicialmente
-    updateButtonFilterZIndex();
-
     // Observar cambios en el DOM utilizando MutationObserver
     const observer = new MutationObserver(updateButtonFilterZIndex);
     observer.observe(document.body, { childList: true, subtree: true });
 
     // Obtener los botones y el porcentaje de recorrido
     const buttonsContainer = document.querySelector('.main__container__newsButtons');
-    const scrollThreshold = 0.03;
+    const scrollThreshold = 0.035;
 
     // Función para verificar el porcentaje de recorrido y mostrar/ocultar los botones
     function checkScrollThreshold() {
