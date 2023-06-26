77d202015d3364a6cd2ba162a1cf9d981fa06e54
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-26 15:52:15 -0300

    mult294

--- MUL294/index.js
+++ MUL294/index.js
@@ -1,12 +1,12 @@
 function cambiarTextoBoton() {
     let button = document.querySelector('.result-option__book-button');
-  
+
     if (button) {
-      button.textContent = 'Comprar';
-      return;
+        button.textContent = 'Comprar';
+        return;
     }
 
     setTimeout(cambiarTextoBoton, 100);
-  }
+}
 
-  cambiarTextoBoton();
+cambiarTextoBoton();
