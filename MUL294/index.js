48ab9b54ab3b6fe0823f7ce0450535a6d18227cb
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-26 16:32:08 -0300

    mult294

--- MUL294/index.js
+++ MUL294/index.js
@@ -8,5 +8,43 @@ function cambiarTextoBoton() {
 
     setTimeout(cambiarTextoBoton, 100);
 }
-
 cambiarTextoBoton();
+
+function findAndAttachClickEvent() {
+    const link = document.querySelector('.info-card__action-item');
+
+    link.addEventListener('click', () => {
+        const app = document.querySelector('#root');
+        app.querySelector('button').click();
+    });
+}
+
+function App() {
+    const [modalIsOpen, setModalIsOpen] = React.useState(false);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    React.useEffect(() => {
+        findAndAttachClickEvent();
+    }, []);
+
+    return (
+        <div>
+            <button onClick={openModal}>Abrir Modal</button>
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                <h2>Título del Modal</h2>
+                <p>Contenido del modal...</p>
+                <button onClick={closeModal}>Cerrar Modal</button>
+            </Modal>
+        </div>
+    );
+}
+
+const targetDivDesktop = document.querySelector('#main-content');
+ReactDOM.render(<App />, targetDivDesktop);
