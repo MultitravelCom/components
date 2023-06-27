204dde0104e9383c21476510ff7ca91b744f496f
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-27 15:36:38 -0300

    mul294

--- MUL294/index.js
+++ MUL294/index.js
@@ -8,7 +8,6 @@ function cambiarTextoBoton() {
 
     setTimeout(cambiarTextoBoton, 100);
 }
-
 function moverDescripcionAlModal() {
     let intervalId = setInterval(function() {
         const descriptionDiv = document.querySelector('.js-result-package-option__hotel-description');
@@ -79,11 +78,74 @@ function abrirVentanaModal() {
         }
     }, 100);
 }
+function agregarTextosYModales() {
+    function abrirModal(titulo, contenido) {
+      const modal = document.createElement('div');
+      modal.classList.add('modal');
+      modal.style.display = 'block';
+  
+      const modalContent = document.createElement('div');
+      modalContent.classList.add('modal-content');
+  
+      const modalTitle = document.createElement('h2');
+      modalTitle.textContent = titulo;
+  
+      const modalText = document.createElement('p');
+      modalText.textContent = contenido;
+  
+      modalContent.appendChild(modalTitle);
+      modalContent.appendChild(modalText);
+      modal.appendChild(modalContent);
+  
+      document.body.appendChild(modal);
+  
+      // Agregar evento de clic para cerrar la ventana modal al hacer clic fuera de ella
+      modal.addEventListener('click', function (event) {
+        if (event.target === modal) {
+          modal.style.display = 'none';
+        }
+      });
+    }
+  
+    function agregarTextos() {
+      const divResultado = document.querySelector('.result-package-result--selected.package-result--master');
+  
+      const verVuelo = document.createElement('span');
+      verVuelo.classList.add('ver-vuelo');
+      verVuelo.textContent = 'Ver vuelo';
+  
+      const verServicio = document.createElement('span');
+      verServicio.classList.add('ver-servicio');
+      verServicio.textContent = 'Ver servicio';
+  
+      divResultado.appendChild(verVuelo);
+      divResultado.appendChild(verServicio);
+    }
+  
+    agregarTextos();
+  
+    const verVuelo = document.querySelector('.ver-vuelo');
+    const verServicio = document.querySelector('.ver-servicio');
+  
+    if (verVuelo) {
+      verVuelo.addEventListener('click', function () {
+        abrirModal('Modal de vuelo', 'Contenido del modal de vuelo.');
+      });
+    }
+  
+    if (verServicio) {
+      verServicio.addEventListener('click', function () {
+        abrirModal('Modal de servicio', 'Contenido del modal de servicio.');
+      });
+    }
+  }
+  
 document.addEventListener("DOMContentLoaded", function () {
     cambiarTextoBoton();
     moverDescripcionAlModal()
     abrirVentanaModal();
     cambiarTextoBoton();
+    agregarTextosYModales();
 
     const boton = document.querySelector('.result-option__change-button');
     boton.addEventListener('click', cambiarTextoBoton);
