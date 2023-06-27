da233abe7a8414dcac216fcdeb13c365950a7d84
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-27 13:16:30 -0300

    mul294

--- MUL294/index.js
+++ MUL294/index.js
@@ -19,47 +19,48 @@ function cambiarTextoBoton() {
 // }
 
 function abrirVentanaModal() {
-    const link = document.querySelector('.info-card__action-item');
+    let intervalId = setInterval(function() {
+        const link = document.querySelector('.info-card__action-item');
 
-    if (link) {
-        console.log('Enlace encontrado:', link);
+        if (link) {
+            clearInterval(intervalId); // Finalizar el intervalo
+            console.log('Enlace encontrado:', link);
 
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
 
-            let modal = document.querySelector('#miModal');
+                let modal = document.querySelector('#miModal');
 
-            if (!modal) {
-                modal = document.createElement('div');
-                modal.classList.add('modal');
-                modal.setAttribute('id', 'miModal');
-                modal.innerHTML = `
-                    <div class="modal-content">
-                        <h2>Ventana Modal</h2>
-                        <p>Contenido de la ventana modal.</p>
-                    </div>
-                `;
+                if (!modal) {
+                    modal = document.createElement('div');
+                    modal.classList.add('modal');
+                    modal.setAttribute('id', 'miModal');
+                    modal.innerHTML = `
+                        <div class="modal-content">
+                            <h2>Ventana Modal</h2>
+                            <p>Contenido de la ventana modal.</p>
+                        </div>
+                    `;
 
-                document.body.appendChild(modal);
+                    document.body.appendChild(modal);
 
-                    
-                // moverDescripcionAlModal(modal.querySelector('.modal-content'));
-            }
-
-            modal.style.display = 'block';
-
-            // Agregar evento de clic para cerrar la ventana modal al hacer clic fuera de ella
-            modal.addEventListener('click', function (event) {
-                if (event.target === modal) {
-                    modal.style.display = 'none';
+                    // moverDescripcionAlModal(modal.querySelector('.modal-content'));
                 }
-            });
-        });
-    } else {
-        console.log('Enlace no encontrado');
-    }
-}
 
+                modal.style.display = 'block';
+
+                // Agregar evento de clic para cerrar la ventana modal al hacer clic fuera de ella
+                modal.addEventListener('click', function (event) {
+                    if (event.target === modal) {
+                        modal.style.display = 'none';
+                    }
+                });
+            });
+        } else {
+            console.log('Enlace no encontrado');
+        }
+    }, 100);
+}
 document.addEventListener("DOMContentLoaded", function () {
     cambiarTextoBoton();
     abrirVentanaModal();
