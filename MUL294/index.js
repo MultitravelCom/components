841d505281883e829b4c2fc5cd9676c987051bd3
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-06-27 11:16:43 -0300

    mul294

--- MUL294/index.js
+++ MUL294/index.js
@@ -8,48 +8,38 @@ function cambiarTextoBoton() {
 
     setTimeout(cambiarTextoBoton, 100);
 }
-function initializeModal() {
-    const myLink = document.querySelector(".js-result-selected-action--extended-hotel-info");
+function abrirVentanaModal() {
+    // Obtén la referencia al elemento <a> mediante su clase o cualquier otro selector válido
+    const link = document.querySelector('.info-card__action-item');
 
-    function openModal() {
-        console.log("Se ha hecho clic en el botón");
-        const modal = document.createElement("div");
-        modal.id = "myModal";
-        modal.className = "modal";
-        modal.innerHTML = `
-        <div class="modal-content">
-          <span class="close">&times;</span>
-          <h2>Título del Modal</h2>
-          <p>Contenido del Modal</p>
-        </div>
-      `;
+    // Añade un evento de clic al enlace
+    link.addEventListener('click', function (event) {
+        event.preventDefault(); // Evita que se realice la acción predeterminada del enlace
+
+        // Crea y muestra la ventana modal
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.style.display = 'block';
+
+        const modalContent = document.createElement('div');
+        modalContent.classList.add('modal-content');
+
+        const modalTitle = document.createElement('h2');
+        modalTitle.textContent = 'Ventana Modal';
+
+        const modalText = document.createElement('p');
+        modalText.textContent = 'Contenido de la ventana modal.';
+
+        modalContent.appendChild(modalTitle);
+        modalContent.appendChild(modalText);
+        modal.appendChild(modalContent);
 
         document.body.appendChild(modal);
-
-        const closeBtn = modal.getElementsByClassName("close")[0];
-        closeBtn.onclick = closeModal;
-
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                closeModal();
-            }
-        };
-    }
-
-    function closeModal() {
-        const modal = document.getElementById("myModal");
-        if (modal) {
-            modal.parentNode.removeChild(modal);
-        }
-    }
-
-    if (myLink) {
-        myLink.onclick = openModal;
-    }
+    });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    initializeModal();
+document.addEventListener("DOMContentLoaded", function () {
+    abrirVentanaModal();
     cambiarTextoBoton();
-  });
-  
+});
+
