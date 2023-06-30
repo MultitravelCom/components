7fcd34c1f6027d9b74ec73c9e1bef52456f2bfa8
Multitravel.com <ignacio.peralta@multitravel.com>
2023-06-30 16:23:29 -0300

    copy

--- MUL294/index.js
+++ MUL294/index.js
@@ -1,63 +1,87 @@
 // Objeto para almacenar los modales creados
 const modalesCreados = {};
 
+// Función para crear los modales al cargar la página
+function crearModalesIniciales() {
+    const modalesIniciales = [
+        {
+            titulo: 'Modal de vuelo',
+            contenido: '.result-option__extended-info',
+            selectorTexto: 'Ver vuelo'
+        },
+        {
+            titulo: 'Modal de servicio',
+            contenido: 'Contenido del modal de servicio.',
+            selectorTexto: 'Ver servicio'
+        }
+    ];
+
+    modalesIniciales.forEach(modalInicial => {
+        const { titulo, contenido, selectorTexto } = modalInicial;
+        const modalId = selectorTexto.replace(/\s/g, '-').toLowerCase(); // Convertir el texto del selector a formato ID
+        let modal = modalesCreados[modalId];
+
+        if (!modal) {
+            modal = document.createElement('div');
+            modal.classList.add('modal');
+            modal.setAttribute('id', modalId);
+
+            const modalContent = document.createElement('div');
+            modalContent.classList.add('modal-content');
+
+            const modalTitle = document.createElement('h2');
+            modalTitle.textContent = titulo; // Establecer el título del modal
+
+            modalContent.appendChild(modalTitle);
+            modal.appendChild(modalContent);
+            document.body.appendChild(modal);
+
+            // Agregar evento de clic para cerrar el modal al hacer clic fuera de él
+            modal.addEventListener('click', function (event) {
+                if (event.target === modal) {
+                    cerrarModal();
+                }
+            });
+
+            // Almacenar el modal creado en el objeto modalesCreados
+            modalesCreados[modalId] = modal;
+        }
+
+        const modalContent = modal.querySelector('.modal-content');
+        modalContent.textContent = ''; // Limpiar el contenido existente del modal
+
+        // Obtener el contenido del selector especificado en el modal inicial
+        const contenidoModal = document.querySelector(contenido);
+        if (contenidoModal) {
+            const contenidoClonado = contenidoModal.cloneNode(true);
+            modalContent.appendChild(contenidoClonado);
+        }
+    });
+}
+
 // Función para abrir el modal
-function abrirVentanaModal(titulo, contenido, selectorTexto) {
-    if (!selectorTexto) {
-        console.error('No se ha proporcionado el selector de texto.');
-        return;
-    }
+function abrirVentanaModal(modalId) {
+    const modal = modalesCreados[modalId];
 
-    const modalId = selectorTexto.replace(/\s/g, '-').toLowerCase(); // Convertir el texto del selector a formato ID
-    let modal = modalesCreados[modalId];
-
-    if (!modal) {
-        modal = document.createElement('div');
-        modal.classList.add('modal');
-        modal.setAttribute('id', modalId);
-
-        const modalContent = document.createElement('div');
-        modalContent.classList.add('modal-content');
-
-        const modalTitle = document.createElement('h2');
-        modalTitle.textContent = titulo; // Establecer el título del modal
-
-        modalContent.appendChild(modalTitle);
-        modal.appendChild(modalContent);
-        document.body.appendChild(modal);
-
-        // Agregar evento de clic para cerrar el modal al hacer clic fuera de él
-        modal.addEventListener('click', function (event) {
-            if (event.target === modal) {
-                cerrarModal();
-            }
-        });
-
-        // Almacenar el modal creado en el objeto modalesCreados
-        modalesCreados[modalId] = modal;
-    }
-
-    const modalContent = modal.querySelector('.modal-content');
-    modalContent.textContent = ''; // Limpiar el contenido existente del modal
-
-    // Obtener el contenido del selector result-option__extended-info
-    const contenidoModal = document.querySelector(selectorTexto);
-    if (contenidoModal) {
-        const contenidoClonado = contenidoModal.cloneNode(true);
-        modalContent.appendChild(contenidoClonado);
-    }
-
-    // Mostrar el modal
-    modal.style.display = 'flex';
-
-    // Función para cerrar el modal
-    function cerrarModal() {
-        modal.style.display = 'none';
-        modal.removeEventListener('click', cerrarModal);
+    if (modal) {
+        modal.style.display = 'flex';
+    } else {
+        console.error(`No se encontró el modal con el ID: ${modalId}`);
     }
 }
 
-//********************** */
+// Función para cerrar el modal
+function cerrarModal(modalId) {
+    const modal = modalesCreados[modalId];
+
+    if (modal) {
+        modal.style.display = 'none';
+    } else {
+        console.error(`No se encontró el modal con el ID: ${modalId}`);
+    }
+}
+
+// Función para cambiar el texto del botón
 function cambiarTextoBoton() {
     const botonDataProduct = document.querySelector('.result-option__change-button');
 
@@ -102,7 +126,7 @@ function agregarTextos() {
                     resultOptionElements[0].appendChild(divVerVuelo);
 
                     divVerVuelo.addEventListener('click', function () {
-                        abrirVentanaModal('Modal de vuelo', '.result-option__extended-info', 'Ver vuelo');
+                        abrirVentanaModal('ver-vuelo');
                     });
                 }
 
@@ -110,7 +134,7 @@ function agregarTextos() {
                     resultOptionElements[1].appendChild(divVerServicio);
 
                     divVerServicio.addEventListener('click', function () {
-                        abrirVentanaModal('Modal de servicio', 'Contenido del modal de servicio.', 'Ver servicio');
+                        abrirVentanaModal('ver-servicio');
                     });
                 }
             }
@@ -119,7 +143,7 @@ function agregarTextos() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+    crearModalesIniciales();
     cambiarTextoBoton();
-    agregarTextos()
-  });
-  
+    agregarTextos();
+});
