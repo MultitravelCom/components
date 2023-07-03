67274048cb1b13a603deb5a47811bc6cf09ece91
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-03 16:03:03 -0300

    mult294

--- MUL294/index.js
+++ MUL294/index.js
@@ -10,7 +10,7 @@ function moverContenidoVuelos() {
     const modalVuelos = document.getElementById('ver-vuelo');
 
     if (contenidoModal && modalVuelos) {
-        const modalContent = modalVuelos.querySelector('.modal-content');
+        const modalContent = modalVuelos.querySelector('#ver-vuelo .modal-content');
         modalContent.textContent = '';
         modalContent.appendChild(contenidoModal);
     } else {
