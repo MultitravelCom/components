7c67d362308aea6f930854cd14ef3aafa445e9a7
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-05 17:33:40 -0300

    pic

--- MULT245/index.js
+++ MULT245/index.js
@@ -16,9 +16,9 @@ async function removeDataTarget() {
     const elements = parentElement.querySelectorAll('.results-list__item');
 
     elements.forEach((element) => {
-        const imageHolder = element.querySelector('#results-list > div.results-list__body.js-results-list-placeholder > div > div:nth-child(1) > article');
-        if (imageHolder ) {
-            imageHolder .classList.remove('result');
+        const imageHolder = element.querySelector('.result');
+        if (imageHolder) {
+            imageHolder.classList.remove('result');
         }
     });
 }
