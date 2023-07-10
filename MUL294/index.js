7357eb292bc5dc8b7886fe9c0ff22d59e7181279
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-10 16:21:24 -0300

    mult294

--- MUL294/index.js
+++ MUL294/index.js
@@ -18,77 +18,77 @@ function moverContenidoVuelos() {
     }
 }
 function moverContenidoServicio() {
-    let intervalId = setInterval(function() {
-      let modalContent = document.querySelector("#ver-servicio .modal-content");
-      let elements = document.querySelectorAll(".result-option__extended-info.result-option__extended-info--hotel");
-  
-      if (modalContent && elements.length > 0) {
-        elements.forEach(function(element) {
-          modalContent.appendChild(element);
-        });
-        clearInterval(intervalId); // Detener el setInterval una vez que se hayan movido los elementos
-      }
+    let intervalId = setInterval(function () {
+        let modalContent = document.querySelector("#ver-servicio .modal-content");
+        let elements = document.querySelectorAll(".result-option__extended-info.result-option__extended-info--hotel");
+
+        if (modalContent && elements.length > 0) {
+            elements.forEach(function (element) {
+                modalContent.appendChild(element);
+            });
+            clearInterval(intervalId); // Detener el setInterval una vez que se hayan movido los elementos
+        }
     }, 100);
-  }
+}
 
 // Objeto para almacenar los modales creados
 const modalesCreados = {};
 // Función para crear los modales al cargar la página
 function crearModalesIniciales() {
     const modalesIniciales = [
-      {
-        titulo: 'Modal de vuelo',
-        selectorTexto: 'Ver vuelo',
-      },
-      {
-        titulo: 'Modal de servicio',
-        selectorTexto: 'Ver servicio',
-      }
+        {
+            titulo: 'Modal de vuelo',
+            selectorTexto: 'Ver vuelo',
+        },
+        {
+            titulo: 'Modal de servicio',
+            selectorTexto: 'Ver servicio',
+        }
     ];
-  
+
     modalesIniciales.forEach(modalInicial => {
-      const { titulo, contenido, selectorTexto } = modalInicial;
-      const modalId = selectorTexto.replace(/\s/g, '-').toLowerCase(); // Convertir el texto del selector a formato ID
-      let modal = modalesCreados[modalId];
-  
-      if (!modal) {
-        modal = document.createElement('div');
-        modal.classList.add('modal');
-        modal.setAttribute('id', modalId);
-  
-        const modalContent = document.createElement('div');
-        modalContent.classList.add('modal-content');
-  
-        const modalTitle = document.createElement('h2');
-        modalTitle.textContent = titulo; // Establecer el título del modal
-  
-        const modalButton = document.createElement('button');
-        modalButton.textContent = 'Cerrar'; // Texto del botón
-        modalButton.addEventListener('click', function() {
-          cerrarModal(modalId); // Llamar a la función para cerrar el modal al hacer clic en el botón
-        });
-  
-        modalContent.appendChild(modalTitle);
-        modalContent.appendChild(modalButton);
-        modal.appendChild(modalContent);
-        document.body.appendChild(modal);
-  
-        // Agregar evento de clic para cerrar el modal al hacer clic fuera de él
-        modal.addEventListener('click', function(event) {
-          if (event.target === modal) {
-            cerrarModal(modalId);
-          }
-        });
-  
-        // Almacenar el modal creado en el objeto modalesCreados
-        modalesCreados[modalId] = modal;
-      }
-  
-      const modalContent = modal.querySelector('.modal-content');
-      modalContent.textContent = contenido; // Establecer el contenido del modal
+        const { titulo, contenido, selectorTexto } = modalInicial;
+        const modalId = selectorTexto.replace(/\s/g, '-').toLowerCase(); // Convertir el texto del selector a formato ID
+        let modal = modalesCreados[modalId];
+
+        if (!modal) {
+            modal = document.createElement('div');
+            modal.classList.add('modal');
+            modal.setAttribute('id', modalId);
+
+            const modalContent = document.createElement('div');
+            modalContent.classList.add('modal-content');
+
+            const modalTitle = document.createElement('h2');
+            modalTitle.textContent = titulo; // Establecer el título del modal
+
+            const modalButton = document.createElement('button');
+            modalButton.textContent = 'Cerrar'; // Texto del botón
+            modalButton.addEventListener('click', function () {
+                cerrarModal(modalId); // Llamar a la función para cerrar el modal al hacer clic en el botón
+            });
+
+            modalContent.appendChild(modalTitle);
+            modalContent.appendChild(modalButton);
+            modal.appendChild(modalContent);
+            document.body.appendChild(modal);
+
+            // Agregar evento de clic para cerrar el modal al hacer clic fuera de él
+            modal.addEventListener('click', function (event) {
+                if (event.target === modal) {
+                    cerrarModal(modalId);
+                }
+            });
+
+            // Almacenar el modal creado en el objeto modalesCreados
+            modalesCreados[modalId] = modal;
+        }
+
+        const modalContent = modal.querySelector('.modal-content');
+        modalContent.textContent = contenido; // Establecer el contenido del modal
     });
-  }
-  
+}
+
 // Función para abrir el modal
 function abrirVentanaModal(modalId) {
     const modal = modalesCreados[modalId];
@@ -170,23 +170,24 @@ function agregarTextos() {
     }, 150);
 }
 function observarCambios() {
-    const elementosObservados = document.querySelectorAll('.result.package-result--selected.package-result--master');
-  
+    const elementosObservados = document.querySelectorAll('.js-package-selection-placeholder');
+
     // Crear un observer con una función de callback
-    const observer = new MutationObserver(function() {
-      // Ejecutar las funciones nuevamente
-      crearModalesIniciales();
-      cambiarTextoBoton();
-      agregarTextos();
-      moverContenidoVuelos();
-      moverContenidoServicio();
+    const observer = new MutationObserver(function () {
+        console.log('Se detectaron cambios en el selector.');
+        // Ejecutar las funciones nuevamente
+        crearModalesIniciales();
+        cambiarTextoBoton();
+        agregarTextos();
+        moverContenidoVuelos();
+        moverContenidoServicio();
     });
-  
+
     // Observar los cambios en cada elemento
-    elementosObservados.forEach(function(elemento) {
-      observer.observe(elemento, { attributes: true, childList: true, subtree: true });
+    elementosObservados.forEach(function (elemento) {
+        observer.observe(elemento, { attributes: true, childList: true, subtree: true });
     });
-  }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     crearModalesIniciales();
