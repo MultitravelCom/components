71b9527f87dbae06b79189a747679c288722b9f3
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-07-11 19:26:04 -0300

    mult297

--- MULT297/index.js
+++ MULT297/index.js
@@ -1,41 +1,40 @@
 function cambiarTextoBoton() {
     let boton = document.querySelector('#main-content > div > article > section.details-content > div.details-card__top > div > div.details-card__price > div > button');
     if (boton) {
-      let idioma = navigator.language || navigator.userLanguage; // Obtiene el idioma del navegador del usuario
-      let traduccion;
-  
-      switch (idioma) {
-        case 'es':
-          traduccion = 'Ver opciones';
-          break;
-        case 'pt':
-          traduccion = 'ver opções'; 
-          break;
-        default:
-          traduccion = 'Ver opciones';
-      }
-  
-      boton.textContent = traduccion;
+        let idioma = navigator.language || navigator.userLanguage; // Obtiene el idioma del navegador del usuario
+        let traduccion;
+
+        switch (idioma) {
+            case 'es':
+                traduccion = 'Ver opciones';
+                break;
+            case 'pt':
+                traduccion = 'ver opções';
+                break;
+            default:
+                traduccion = 'Ver opciones';
+        }
+
+        boton.textContent = traduccion;
     }
-  }
+}
 
 
 const CompartirAlojamiento = () => {
     const [openModal, setOpenModal] = React.useState(false);
     return (
         <>
-            <div className="cont">
-                <ButtonModalShare
-                    onClick={() => setOpenModal(true)}
-                    style="main__container__widget__share"
-                >
-                    <div className="main__container__share js-social-share" id="hotelResult">
-                        <span className="glyphicon glyphicon-share share__icon"></span>
-                        <span className="share__text">Compartí este alojamiento ahora</span>
-                    </div>
-                </ButtonModalShare>
-                <ModalShare open={openModal} onClose={() => setOpenModal(false)} />
-            </div>
+            <ButtonModalShare
+                onClick={() => setOpenModal(true)}
+                style="main__container__widget__share"
+            >
+                <div className="main__container__share js-social-share" id="hotelResult">
+                    <span className="glyphicon glyphicon-share share__icon"></span>
+                    <span className="share__text">Compartí este alojamiento ahora</span>
+                </div>
+            </ButtonModalShare>
+            <ModalShare open={openModal} onClose={() => setOpenModal(false)} />
+
         </>
     )
 }
@@ -50,7 +49,6 @@ const checkAndRender = async () => {
 
     infoCardContents.forEach(infoCardContent => {
         const nuevoDiv = document.createElement('div');
-
         infoCardContent.appendChild(nuevoDiv);
 
 
