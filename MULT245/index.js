b1be65b4110790d76bdba4786533824e27b35541
Multitravel.com <ignacio.peralta@multitravel.com>
2023-07-28 11:43:12 -0300

    result

--- MULT245/index.js
+++ MULT245/index.js
@@ -220,7 +220,7 @@ function aplicarModificaciones(resultsListPage) {
     changeCopyButton(resultsListPage);
     aplicarEstiloSegunLongitud();
     removeClassResultInHotelResults();
-    agregarClassResultInHotelResults()
+    // agregarClassResultInHotelResults();
 };
 
 function observarCambiosCheckAndRender() {
