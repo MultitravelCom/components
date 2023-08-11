b6499f6fcc5661c0096251cedb6c52ef85b46d4e
Multitravel.com <ignacio.peralta@multitravel.com>
2023-08-11 17:00:31 -0300

    poc zonecode

--- Poc-ZoneCode/index.js
+++ Poc-ZoneCode/index.js
@@ -1,24 +1,3 @@
-// Funcion para obtener ZoneID.
-const getZoneId = () => {
-    const interval = setInterval(() => {
-        const dataValueElement = document.querySelector('.zone-selector-value');
-        
-        if (dataValueElement) {
-            const dataValue = dataValueElement.value;
-            const numericValue = parseInt(dataValue);
-
-            if (numericValue === 48656) {
-                clearInterval(interval);
-                console.log("Bariloche. ZonaCode:", numericValue);
-                renderBanner();
-            } else {
-                clearInterval(interval);
-                console.log("Not Bariloche. ZonaCode:", numericValue);
-            }
-        }
-    }, 1000);
-};
-
 const renderBanner = () => {
     const mainContentElement = document.getElementById('main-content');
 
@@ -39,5 +18,25 @@ const BannerTopTravelSale = () =>{
     );
 };
 
+// Funcion para obtener ZoneID.
+const getZoneId = () => {
+    const interval = setInterval(() => {
+        const dataValueElement = document.querySelector('.zone-selector-value');
+        
+        if (dataValueElement) {
+            const dataValue = dataValueElement.value;
+            const numericValue = parseInt(dataValue);
+
+            if (numericValue === 48656) {
+                clearInterval(interval);
+                console.log("Bariloche. ZonaCode:", numericValue);
+                renderBanner();
+            } else {
+                clearInterval(interval);
+                console.log("Not Bariloche. ZonaCode:", numericValue);
+            }
+        }
+    }, 1000);
+};
 
 getZoneId();
