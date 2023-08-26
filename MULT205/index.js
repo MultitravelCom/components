d01345127dec9b404e9ee9c1f1b0f03c1068479d
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-26 13:30:30 -0300

    auto change background redes

--- MULT205/index.js
+++ MULT205/index.js
@@ -211,7 +211,7 @@ const ModalCupones = ({ isOpen, onClose }) => {
                         <span className="close-modal-cupon" onClick={onClose}>X</span>
                     </div>
                     <div className="row modal-content__cupones-row">
-                        {shouldShowCupones ?? <ComponenteCupones />}
+                        {shouldShowCupones ? <ComponenteCupones /> : null}
                     </div>
                 </div>
             </div>
