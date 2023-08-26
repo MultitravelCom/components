5a732b50d2536664ae58d2139717456c928a35de
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-26 13:08:03 -0300

    auto change background redes

--- MULT205/index.js
+++ MULT205/index.js
@@ -164,8 +164,8 @@ function ComponenteCupones() {
 // Modal
 const ModalCupones = ({ isOpen, onClose }) => {
 
-    const startDate = new Date(2023, 7, 25, 23, 30); // 27 de Agosto a las 23:30
-    const endDate = new Date(2023, 8, 2, 23, 30);   // 2 de Septiembre a las 23:30
+    const startDate = new Date(2023, 8, 25, 23, 30); // 27 de Agosto a las 23:30
+    const endDate = new Date(2023, 9, 2, 23, 30);   // 2 de Septiembre a las 23:30
     const shouldShowCupones = isWithinDateRange(startDate, endDate);
 
 
