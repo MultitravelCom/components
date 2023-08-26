b88bc0e4c2c78b410c1eb55989c38a91da407461
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-26 14:32:11 -0300

    banners auto

--- MULT324/index.js
+++ MULT324/index.js
@@ -175,7 +175,7 @@ const Loader = () => {
     );
 };
 function shouldShowEvent() {
-    const startDate = new Date("2023-08-27T23:59:00");
+    const startDate = new Date("2023-08-25T23:59:00");
     const endDate = new Date("2023-09-02T23:59:00");
     const now = new Date();
 
