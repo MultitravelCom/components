7c830fb5fac89d00fc64451781a3047a31742e21
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-26 20:30:55 -0300

    hotels result

--- MULT245/modalShare.js
+++ MULT245/modalShare.js
@@ -115,7 +115,7 @@ const BannerMensageCardApp = () => {
                 <BannerMensageCard text_p={"Pagá hasta en 12 cuotas fijas"} />
             )}
 
-            {taxIncludedTrue && (
+            {taxIncludedTrue && !travelSaleTrue && (
                 <BannerMensageCard text_p={"Comprá ahora y congela el precio en pesos"} />
             )}
         </>
