e6d5b9dadc7ea2c713006e9d577f6b6ead447f2e
Ignacio Peralta <javierignacio.peralta@gmail.com>
2023-08-26 21:03:12 -0300

    hotels result

--- MULT245/modalShare.js
+++ MULT245/modalShare.js
@@ -11,8 +11,8 @@ const zonasTravelSale = [
 //   *************************************************************
 // ********************** Timer *****************************
 function shouldShowEvent() {
-    const startDate = new Date("2023-08-27T23:00:00");
-    const endDate = new Date("2023-09-02T23:00:00");
+    const startDate = new Date("2023-08-27T22:00:00");
+    const endDate = new Date("2023-09-02T23:30:00");
     const now = new Date();
 
     return now >= startDate && now <= endDate;
