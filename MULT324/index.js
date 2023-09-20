170e1370d1562bb16c77e49bd4da23885bfa7f3e
Multitravel.com <ignacio.peralta@multitravel.com>
2023-09-20 12:19:40 -0300

    strapi landing caribe

--- MULT324/index.js
+++ MULT324/index.js
@@ -94,6 +94,21 @@ async function fetchDataFromAPI() {
     }
 }
 
+async function fetchDataFromAPIPrice() {
+    try {
+        const response = await fetch('https://32tpwbxjq7.us-east-1.awsapprunner.com/api/lading-brasils');
+        if (!response.ok) {
+            throw new Error('No se pudo obtener los datos de la API');
+        }
+        const responseDataPrice = await response.json();
+
+        return responseDataPrice;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
 // ************************************************
 // Filter
 function filtrarDestinos(destinos, nombreDestino) {
@@ -296,6 +311,7 @@ const Card = ({ destinos, onContactClick }) => {
     const [openModal, setOpenModal] = React.useState(false);
     const [buttonSwitch, setButtonSwitch] = React.useState("B");
     const [data, setData] = React.useState([]);
+    const [pricesByDestino, setPricesByDestino] = React.useState({});
 
 
     const handleBannerClick = () => {
@@ -346,6 +362,39 @@ const Card = ({ destinos, onContactClick }) => {
 
         fetchData();
     }, []);
+    React.useEffect(() => {
+        const fetchDataPrecio = async () => {
+            try {
+                const responseData = await fetchDataFromAPIPrice();
+
+                const prices = responseData.data.reduce((acc, item) => {
+                    const destino = item.attributes.Destino;
+                    const card = item.attributes.Card;
+
+                    if (!acc[destino]) {
+                        acc[destino] = {};
+                    }
+
+                    if (!acc[destino][card]) {
+                        acc[destino][card] = [];
+                    }
+
+                    acc[destino][card].push({
+                        Tarifa_Temporada_Alta: item.attributes.Tarifa_Temporada_Alta,
+                        Tarifa_Temporada_Baja: item.attributes.Tarifa_Temporada_Baja,
+                    });
+
+                    return acc;
+                }, {});
+                setPricesByDestino(prices);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+
+        fetchDataPrecio();
+    }, []);
 
     return (
         <>
@@ -374,8 +423,14 @@ const Card = ({ destinos, onContactClick }) => {
                                     />
                                 </picture>
                                 <div className="main_container_priceStyle">
-                                    <div className="priceStyle left">{destino.priceBaja}</div>
-                                    <div className="priceStyle right">{destino.price}</div>
+                                    {pricesByDestino[destino.destino] && (
+                                        pricesByDestino[destino.destino][destino.cardOrden].map((tarifa, index) => (
+                                            <div key={index} className="main_container_priceStyle">
+                                                <div className="priceStyle left">${tarifa.Tarifa_Temporada_Baja.toLocaleString().replace(/,/g, '.')}</div>
+                                                <div className="priceStyle right">$ {tarifa.Tarifa_Temporada_Alta.toLocaleString().replace(/,/g, '.')}</div>
+                                            </div>
+                                        ))
+                                    )}
                                 </div>
                                 <div className="main__container__buttonsCars">
                                     <>
