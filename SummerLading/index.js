9a953c8f6005044739e1270ea28f9d1a05656ee4
Multitravel.com <ignacio.peralta@multitravel.com>
2023-11-10 18:16:04 -0300

    lading summer

--- SummerLading/index.js
+++ SummerLading/index.js
@@ -383,72 +383,71 @@ const Card = ({ cards, btnStyles }) => {
 const CardContainer = ({ btnStyles, onContactClick }) => {
     const { title, btnLeft, btnRight, carrusel, destino, cards } = btnStyles || {};
 
+    const setupGlider = () => {
+        console.log('Configurando Glider...');
+        const btnLeftElement = document.querySelector(`.${btnLeft}`);
+        const btnRightElement = document.querySelector(`.${btnRight}`);
+        console.log("btnLeftElement---->", btnLeftElement)
+        console.log("btnLeftElement---->", btnRightElement)
+        console.log("carrusel---->", carrusel)
+
+        if (!btnLeftElement || !btnRightElement) {
+            console.error('No se encontraron elementos para los botones de Glider.');
+            return;
+        }
+
+        btnLeftElement.addEventListener('click', function (event) {
+            event.preventDefault();
+        });
+
+        btnRightElement.addEventListener('click', function (event) {
+            event.preventDefault();
+        });
+
+        new Glider(document.querySelector(`.${carrusel}`), {
+            slidesToShow: 1.2,
+            slidesToScroll: 0.5,
+            draggable: true,
+            arrows: {
+                prev: btnLeftElement,
+                next: btnRightElement,
+            },
+            responsive: [
+                {
+                    // screens greater than >= 775px
+                    breakpoint: 450,
+                    settings: {
+                        // Set to `auto` and provide item width to adjust to viewport
+                        slidesToShow: "2.2",
+                        slidesToScroll: "1",
+                    },
+                },
+                {
+                    // screens greater than >= 775px
+                    breakpoint: 760,
+                    settings: {
+                        // Set to `auto` and provide item width to adjust to viewport
+                        slidesToShow: "3.2",
+                        slidesToScroll: "1",
+                    },
+                },
+                {
+                    // screens greater than >= 1024px
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+            rewind: true,
+        });
+
+        console.log('Glider se ha inicializado correctamente.');
+    };
+
     React.useEffect(() => {
         console.log('Ejecutando useEffect...');
-
-        const setupGlider = () => {
-            console.log('Configurando Glider...');
-            const btnLeftElement = document.querySelector(`.${btnLeft}`);
-            const btnRightElement = document.querySelector(`.${btnRight}`);
-            console.log("btnLeftElement---->", btnLeftElement)
-            console.log("btnLeftElement---->", btnRightElement)
-            console.log("carrusel---->", carrusel)
-
-            if (!btnLeftElement || !btnRightElement) {
-                console.error('No se encontraron elementos para los botones de Glider.');
-                return;
-            }
-
-            btnLeftElement.addEventListener('click', function (event) {
-                event.preventDefault();
-            });
-
-            btnRightElement.addEventListener('click', function (event) {
-                event.preventDefault();
-            });
-
-            new Glider(document.querySelector(`.${carrusel}`), {
-                slidesToShow: 1.2,
-                slidesToScroll: 0.5,
-                draggable: true,
-                arrows: {
-                    prev: btnLeftElement,
-                    next: btnRightElement,
-                },
-                responsive: [
-                    {
-                        // screens greater than >= 775px
-                        breakpoint: 450,
-                        settings: {
-                            // Set to `auto` and provide item width to adjust to viewport
-                            slidesToShow: "2.2",
-                            slidesToScroll: "1",
-                        },
-                    },
-                    {
-                        // screens greater than >= 775px
-                        breakpoint: 760,
-                        settings: {
-                            // Set to `auto` and provide item width to adjust to viewport
-                            slidesToShow: "3.2",
-                            slidesToScroll: "1",
-                        },
-                    },
-                    {
-                        // screens greater than >= 1024px
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                        },
-                    },
-                ],
-                rewind: true,
-            });
-
-            console.log('Glider se ha inicializado correctamente.');
-        };
-
         setupGlider(); // Configura el Glider al principio
 
         const observer = new MutationObserver((mutations) => {
@@ -467,7 +466,7 @@ const CardContainer = ({ btnStyles, onContactClick }) => {
         return () => {
             observer.disconnect();
         };
-    }, [btnLeft, btnRight, carrusel]);
+    }, [carrusel]);
 
     return (
         <>
