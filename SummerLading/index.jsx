9c9073571b73e0801632c42d29d2db428c00b851
Multitravel.com <ignacio.peralta@multitravel.com>
2023-11-13 13:23:03 -0300

    update lading summer

--- SummerLading/index.jsx
+++ SummerLading/index.jsx
@@ -164,29 +164,7 @@ const BitrixFormTitle = () => {
   );
 };
 // *************** Style ************************
-const StyledPrice = styled.div`
-  overflow: hidden;
-  margin-bottom: 4rem;
-  margin-top: 1rem;
-  display: flex;
-  position: absolute;
-  width: initial;
-  height: 94px;
-  bottom: 2px;
-  right: 28px;
-  flex-direction: column;
-  justify-content: space-between;
-  @media (width: 425px) {
-    height: 86px;
-    bottom: -9px;
-    right: 26px;
-  }
-  @media (max-width: 768px) {
-    height: 86px;
-    bottom: -9px;
-    right: 26px;
-  }
-`;
+
 // ************** COMPONENTES ********************
 function ButtonLading(props) {
   const svgWA = (
