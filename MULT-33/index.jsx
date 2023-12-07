959b76d34a6e9ec6d8532b9aae0c1587e0c063d2
Multitravel.com <ignacio.peralta@multitravel.com>
2023-12-07 15:47:02 -0300

    mt-33 accordeon navegate site ar/br

--- MULT-33/index.jsx
+++ MULT-33/index.jsx
@@ -1,3 +1,27 @@
+const AccodionNavegateSiteTitle = () => {
+    const [title, setTitle] = React.useState('');
+
+    React.useEffect(() => {
+      const handleTitle = () => {
+        const currentPath = window.location.pathname;
+        const newTitle =
+          currentPath === 'ar.multitravel.com' ? 'Navegar en este sitio:' : currentPath === 'br.multitravel.com' ? 'Navegar no site de:' : 'Navegar en este sitio';
+        setTitle(newTitle);
+      };
+  
+      handleTitle();
+  
+      window.addEventListener('popstate', handleTitle);
+      return () => window.removeEventListener('popstate', handleTitle);
+    }, []);
+
+    return(
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
 const AccodionNavegateSite = () => {
     return(
         <div>
