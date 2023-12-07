d17ddb5f226d29ff9ccb2cc749a460b1d973d392
Multitravel.com <ignacio.peralta@multitravel.com>
2023-12-07 17:02:59 -0300

    hidden package navbar

--- MULT-33/index.jsx
+++ MULT-33/index.jsx
@@ -1,40 +1,43 @@
 const AccodionNavegateSiteTitle = () => {
-    const [title, setTitle] = React.useState('');
+  const [title, setTitle] = React.useState("");
 
-    React.useEffect(() => {
-      const handleTitle = () => {
-        const currentPath = window.location.href;
-        console.log("--currentPath->", currentPath)
-        const newTitle =
-          currentPath === 'https://ar.multitravel.com/' ? 'Navegar en este sitio:' : currentPath === 'https://br.multitravel.com/' ? 'Navegar no site de:' : 'Navegar en este sitio';
-        setTitle(newTitle);
-      };
-  
-      handleTitle();
-  
-      window.addEventListener('popstate', handleTitle);
-      return () => window.removeEventListener('popstate', handleTitle);
-    }, []);
+  React.useEffect(() => {
+    const handleTitle = () => {
+      const currentPath = window.location.href;
+      const isBR = currentURL.includes("https://br.multitravel.com/");
+      const isAR = currentURL.includes("https://ar.multitravel.com/");
 
-    return(
-        <div>
-            <h1>{title}</h1>
-        </div>
-    )
-}
+      console.log("--isAR/isBR->", isAR, isBR);
+      const newTitle = isBR
+        ? 'Navegar en este sitio:'
+        : isAR
+        ? 'Navegar no site de:'
+        : 'Navegar en este sitio:';
+      setTitle(newTitle);
+    };
 
-const AccodionNavegate = () =>{
-    return(
-        <>
+    handleTitle();
 
-        </>
-    )
-}
+    window.addEventListener("popstate", handleTitle);
+    return () => window.removeEventListener("popstate", handleTitle);
+  }, []);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+const AccodionNavegate = () => {
+  return <></>;
+};
 
 const AccodionNavegateSite = () => {
-    return(
-        <AccodionNavegateSiteTitle />
-    )
-}
+  return <AccodionNavegateSiteTitle />;
+};
 
-ReactDOM.render(<AccodionNavegateSite />, document.getElementById("main-footer-checkbooking"));
+ReactDOM.render(
+  <AccodionNavegateSite />,
+  document.getElementById("main-footer-checkbooking")
+);
