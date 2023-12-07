de9f01c87cc382a0a9466b42ce650f6fe9ed494a
Multitravel.com <ignacio.peralta@multitravel.com>
2023-12-07 15:19:43 -0300

    mt-33 accordeon navegate site ar/br

---
+++ MULT-33/index.jsx
@@ -0,0 +1,9 @@
+const AccodionNavegateSite = () => {
+    return(
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<AccodionNavegateSite />, document.getElementById("main-footer-checkbooking"));
