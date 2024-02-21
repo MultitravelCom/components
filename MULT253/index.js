fe4e2235d8bc1e6f382799faf4016eac6516908c
Multitravel.com <ignacio.peralta@multitravel.com>
2024-02-21 17:15:05 -0300

    img card p

--- MULT253/index.js
+++ MULT253/index.js
@@ -152,26 +152,21 @@ function aplicarModificaciones(resultsListPage) {
 
 function findElementByUid(uidToFind) {
     // Selecciona todos los elementos dentro de results-list__page con data-uid
-    const resultElements = document.querySelectorAll('.results-list__page .results-list__item [data-uid]');
-  
-    // Itera sobre cada elemento y verifica si el data-uid coincide con el que estás buscando
-    for (const element of resultElements) {
-      // Obtiene el valor de data-uid
-      const dataUid = element.getAttribute('data-uid');
-  
-      // Verifica si el data-uid coincide con el que estás buscando
-      if (dataUid === uidToFind) {
-        // Si lo encuentra, muestra un mensaje de consola y retorna el elemento
-        console.log(`Elemento con data-uid ${uidToFind} encontrado.`);
-        console.log(element); // Puedes omitir esta línea si no necesitas imprimir el elemento completo
-        return element;
-      }
-    }
-  
+    const articleDataUid = document.querySelectorAll('.results-list__page article[data-uid]');
+
+    articleDataUid.forEach(item => {
+        const dataUid = item.getAttribute('data-uid');
+        console.log(`Valor de data-uid: ${dataUid}`);
+
+        if (dataUid === uidToFind) {
+            console.log(`Elemento con data-uid ${uidToFind} encontrado.`);
+        }
+    });
+
     // Si no se encuentra el elemento, muestra un mensaje de consola
     console.log(`Elemento con data-uid ${uidToFind} no encontrado.`);
-    return null; // Retorna null si no se encuentra el elemento
-  }
+    return null;
+}
 
 function observarCambiosCheckAndRender() {
     const observerConfig = {
