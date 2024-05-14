f9e5d78f06bb2590b03b5144aba76cf89657ce0e
Multitravel.com <ignacio.peralta@multitravel.com>
2024-05-14 15:52:59 -0300

    borrado de console.log

--- MULT245/index.js
+++ MULT245/index.js
@@ -288,6 +288,7 @@ async function replaceImageForUid(resultsListPage) {
 
             // Buscamos el div que contiene el JP correspondiente
             const itemWithDataUid = resultsListPage.querySelector(`[data-uid="GHU@${uid}"]`);
+            console.log("itemWithDataUid", itemWithDataUid)
 
             if (itemWithDataUid) {
                 console.log(`JP found: ${uid}`)
